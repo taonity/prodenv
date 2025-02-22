de0cd3b7609ea52d452793787daf40b4a36b3b29
GeneralTao2 <generaltao.md@gmail.com>
2025-02-23 00:02:20 +0300

    move to nginx

---
+++ nginx/certbot/run.sh
@@ -0,0 +1,11 @@
+docker compose run certbot \
+   certonly \
+     --non-interactive \
+     --agree-tos \
+     --email $1 \
+     --preferred-challenges dns \
+     --authenticator dns-duckdns \
+     --dns-duckdns-credentials /duckdns.ini \
+     --dns-duckdns-propagation-seconds 120  \
+     -d "*.$2" \
+     -d "$2"
